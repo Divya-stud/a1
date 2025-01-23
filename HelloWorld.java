6520783d44061c49ef1f98eadfb8f21a00abccd7
sagarteli101 <sagarteli101@gmail.com>
2025-01-23 22:32:46 +0530

    my second comit

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,7 @@ class HelloWorld {
     {
         System.out.println("Hello, World");
         System.out.println("Hello, World");
-        System.out.println("Hello, World"); //ok divya
+        System.out.println("Hello, World"); 
         
     }
 }
